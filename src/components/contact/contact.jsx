ac81f6f42b96fc32c48e5f8d6a0ff11465049724
walkeriano <admin@MacBook-Pro-de-admin.local>
2025-08-25 13:57:12 +0100

    ready changes

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./contact.module.css";
@@ -9,12 +9,18 @@ import {
   faUser,
   faPhoneVolume,
   faEnvelope,
+  faTriangleExclamation,
 } from "@fortawesome/free-solid-svg-icons";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Contact({ id }) {
   const formShow = useRef(null);
+
+  const formRef = useRef(null);
+  const [isSuccess, setIsSuccess] = useState(false); // Estado para mostrar mensaje de éxito
+  const [isSubmitting, setIsSubmitting] = useState(false); // Estado para deshabilitar el botón mientras se envía
+  const [errorMessage, setErrorMessage] = useState(""); // Estado para manejar errores
 
   useEffect(() => {
     // 🔹 Animación para el texto (entra desde la izquierda)
@@ -36,21 +42,80 @@ export default function Contact({ id }) {
     );
   }, []);
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsSubmitting(true);
+    setErrorMessage(""); // Limpiar cualquier mensaje de error anterior
+
+    const formData = new FormData(event.target);
+    const object = Object.fromEntries(formData);
+
+    // Aquí es donde agregamos la clave de acceso de Web3Forms
+    const accessKey = "285d4c2e-2da7-44f5-8632-15a4707d41a5"; // Asegúrate de incluir las comillas
+    object.access_key = accessKey;
+
+    const json = JSON.stringify(object);
+
+    // Enviar los datos al API de Web3Forms
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      setIsSuccess(true);
+      // Resetea el formulario después de un tiempo
+      setTimeout(() => {
+        formRef.current.reset();
+        setIsSuccess(false);
+      }, 3000);
+    } else {
+      setErrorMessage("Error al enviar el formulario. Inténtalo de nuevo.");
+    }
+
+    setIsSubmitting(false); // Rehabilitar el botón después de la respuesta
+  };
+
   return (
     <section id={id} className={styles.containerContacto}>
-      <section className={styles.containerFlex}>
+      <section ref={formShow} className={styles.containerFlex}>
         <div className={styles.channelsContact}>
           <h3>
             El primer <span>contacto</span>
           </h3>
           <p>Reserva una reunión online o presencial en nuestras oficinas</p>
         </div>
-        <form ref={formShow} className={styles.formContact}>
+        {isSuccess && (
+          <div className={styles.successMessage}>
+            <FontAwesomeIcon
+              className={styles.icon}
+              size="2x"
+              icon={faTriangleExclamation}
+            />
+            <p>¡Gracias! Tu mensaje ha sido enviado con éxito</p>
+          </div>
+        )}
+        {errorMessage && (
+          <div className={styles.errorMessage}>
+            <FontAwesomeIcon
+              className={styles.icon}
+              size="2x"
+              icon={faTriangleExclamation}
+            />
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        <form ref={formRef} onSubmit={onSubmit} className={styles.formContact}>
           <h4>Envíanos tus datos y te contactaremos</h4>
           <section className={styles.inputsArea}>
             <div className={styles.flexInputs}>
               <label htmlFor="">
-                <input type="text" placeholder="Nombre" />
+                <input type="text" placeholder="Nombre" name="name" required />
                 <FontAwesomeIcon
                   className={styles.icon}
                   size="2x"
@@ -58,7 +123,12 @@ export default function Contact({ id }) {
                 />
               </label>
               <label htmlFor="">
-                <input type="text" placeholder="Teléfono" />
+                <input
+                  type="number"
+                  placeholder="Teléfono"
+                  name="name"
+                  required
+                />
                 <FontAwesomeIcon
                   className={styles.icon}
                   size="2x"
@@ -68,7 +138,7 @@ export default function Contact({ id }) {
             </div>
             <div className={styles.flexInputs}>
               <label htmlFor="">
-                <input type="text" placeholder="Email" />
+                <input type="email" placeholder="Email" name="name" required />
                 <FontAwesomeIcon
                   className={styles.icon}
                   size="2x"
@@ -76,7 +146,12 @@ export default function Contact({ id }) {
                 />
               </label>
               <label htmlFor="">
-                <input type="text" placeholder="Servicio" />
+                <input
+                  type="text"
+                  placeholder="Servicio"
+                  name="name"
+                  required
+                />
                 <FontAwesomeIcon
                   className={styles.icon}
                   size="2x"
@@ -85,8 +160,8 @@ export default function Contact({ id }) {
               </label>
             </div>
           </section>
-          <button>
-            <p>enviar</p>
+          <button type="submit" disabled={isSubmitting}>
+            <p>{isSubmitting ? "Enviando..." : "Enviar"}</p>
             <FontAwesomeIcon
               className={styles.icon}
               size="2x"
