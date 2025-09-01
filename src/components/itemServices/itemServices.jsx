f7742d82dfb04959739343a0043a287480f00957
walkeriano <admin@MacBook-Pro-de-admin.local>
2025-09-01 14:00:40 +0100

    changing images

--- src/components/itemServices/itemServices.jsx
+++ src/components/itemServices/itemServices.jsx
@@ -1,67 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useState } from "react";
 import styles from "./itemServices.module.css";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
-gsap.registerPlugin(ScrollTrigger);
-
 export default function ItemServices({ title, description, image }) {
   const [change, setChange] = useState(true);
-  const el = useRef(null);
-
-  useEffect(() => {
-    const element = el.current;
-    gsap.set(element, { opacity: 0, x: 50 });
-
-    const trigger = ScrollTrigger.create({
-      trigger: element,
-      start: "top 80%",
-      end: "bottom 20%",
-      onEnter: () => {
-        gsap.to(element, {
-          opacity: 1,
-          x: 0,
-          duration: 0.8,
-          ease: "power3.out",
-        });
-      },
-      onLeave: () => {
-        gsap.to(element, {
-          opacity: 0,
-          x: -50,
-          duration: 0.5,
-          ease: "power3.in",
-        });
-      },
-      onEnterBack: () => {
-        gsap.to(element, {
-          opacity: 1,
-          x: 0,
-          duration: 0.8,
-          ease: "power3.out",
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to(element, {
-          opacity: 0,
-          x: 50,
-          duration: 0.5,
-          ease: "power3.in",
-        });
-      },
-    });
-
-    return () => {
-      trigger.kill();
-    };
-  }, []);
 
   return (
-    <section ref={el} className={styles.itemReady}>
+    <section className={styles.itemReady}>
       {change ? (
         <section className={styles.containerBtnService}>
           <button className={styles.btnOpen} onClick={() => setChange(false)}>
